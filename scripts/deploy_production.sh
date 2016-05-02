0f421e901dcec4fa3c6b4828e1ccf9031228828d
uzyexe <uzy.exe@gmail.com>
2016-05-02 14:40:23 +0900

    typo

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -2,8 +2,8 @@
 
 PROJECT=$1
 BRANCH=$2
-CIRCLE_TOKEN=$3
+HUB_TOKEN=$3
 
 curl -H "Content-Type: application/json" \
   --data "{\"source_type\": \"Branch\", \"source_name\": \"${BRANCH}\"}" \
-  -X POST "https://registry.hub.docker.com/u/${PROJECT}/trigger/${CIRCLE_TOKEN}/"
+  -X POST "https://registry.hub.docker.com/u/${PROJECT}/trigger/${HUB_TOKEN}/"
